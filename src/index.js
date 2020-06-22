34d1410fa8167f17a1c184df5046def6eb9b2a01
Barun Pandey <barunpandey@Baruns-MacBook-Pro.local>
2020-06-22 21:04:20 +0545

    Skypey application

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./containers/App";
+import * as serviceWorker from "./serviceWorker";
+import store from "./store";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function fancyLog() {
+  console.log("%c Rendered with 👉 color: #FFF");
+  console.log(store.getState());
+}
+
+const render = () => {
+  fancyLog();
+  return ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+store.subscribe(render);
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
